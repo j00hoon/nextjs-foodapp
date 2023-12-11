e71cb58071e26bcd59ae2306bff48dacc80ec7c8
Seunghoon Baik <j00hoon1101@gmail.com>
2023-12-11 18:14:17 -0500

    nextjs-foodapp for practice

---
+++ app/meals/page.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+import classes from './page.module.css';
+import MealsGrid from "../../components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+
+
+
+export const metadata = {
+  title: 'SB Food',
+  description: 'Browse the delicious meal with others!',
+};
+
+
+
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />
+}
+
+
+
+export default function MealHome() {
+
+  
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Delicious meals is for <span className={classes.highlight}>you</span>!</h1>
+        <p>Choose your favorite recipe and cook it!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share your favorite recipe
+          </Link>
+        </p>
+      </header>
+
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+}
+  
